82e9884063a9db471cf1910ea313803dda669499
Mohamed Karam <mk6594182@gmail.com>
2024-11-01 23:01:19 +0200

    sheet 1 problem 3

---
+++ codeforce/Sheet1/C. Simple Calculator.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long x = 0;
+    long long y = 0;
+    cin >> x >> y;
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+    return 0;
+}
