2963c5e8d68b24d58115508aafc9609b16761c95
Mohamed Karam <mk6594182@gmail.com>
2024-11-01 22:37:42 +0200

    sheet 1 problem 2

---
+++ codeforce/Sheet1/Basic Data Types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int integer = 0;
+    long long longNumber;
+    char ch;
+    float flOat;
+    double D;
+    cin >> integer >> longNumber >> ch >> flOat >> D;
+    cout << integer << endl;
+    cout << longNumber << endl;
+    cout << ch << endl;
+    cout << flOat << endl;
+    cout << D << endl;
+
+    return 0;
+}
